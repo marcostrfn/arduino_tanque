2d2d476e703bfd4bea65b5683e59aa54dfe4ef6d
marcostrfn <marcos.trfn@gmail.com>
2019-02-04 22:03:51 +0100

    Update tanque.ino

--- tanque.ino
+++ tanque.ino
@@ -1,34 +1,55 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(4, 2); // RX, TX recorder que se cruzan
 
 
 const int motor_izquierdo = 9;
 const int motor_derecho = 10;
 const int value = 250;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+
+void setup()
+{
+  BT.begin(9600);       // Inicializamos el puerto serie BT que hemos creado
+  Serial.begin(9600);   // Inicializamos  el puerto serie
+
   pinMode(motor_izquierdo, OUTPUT);
   pinMode(motor_derecho, OUTPUT);
-  
+
+  Serial.println("iniciando...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (BT.available())   // Si llega un dato por el puerto BT se envía al monitor serial
+  {
+    char option = BT.read();
+    Serial.write(option);
+    
+    if (option == '0') {
+      digitalWrite(motor_derecho, LOW);
+      digitalWrite(motor_izquierdo, LOW);
+    }
 
-  digitalWrite(motor_derecho, HIGH);
-  delay(1000);
-  digitalWrite(motor_derecho, LOW);
-  delay(1000);
-  digitalWrite(motor_izquierdo, HIGH);
-  delay(1000);
-  digitalWrite(motor_izquierdo, LOW);
-  delay(1000);
+    if (option == '1') {
+      Serial.println("uno");
+      digitalWrite(motor_derecho, HIGH);
+      digitalWrite(motor_izquierdo, HIGH);
+    }
+    if (option == '2') {
+      digitalWrite(motor_derecho, LOW);
+      digitalWrite(motor_izquierdo, HIGH);
+    }
+    if (option == '4') {
+      digitalWrite(motor_derecho, HIGH);
+      digitalWrite(motor_izquierdo, LOW);
+    }
 
-  digitalWrite(motor_derecho, HIGH);
-  digitalWrite(motor_izquierdo, HIGH);
-  delay(1000);
-  digitalWrite(motor_derecho, LOW);
-  digitalWrite(motor_izquierdo, LOW);
-  delay(100);
-  
+
+  }
+
+  if (Serial.available()) // Si llega un dato por el monitor serial se envía al puerto BT
+  {
+    BT.write(Serial.read());
+  }
 }
+
